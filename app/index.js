6645dd9605f0eeb5281834ba1869380b117c7b82
clover <clover_cui@163.com>
2017-04-03 22:21:05 +0800

    预览

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 import _ from 'lodash';
-import j from 'jquery'
+import j from 'jquery';
+import foo from '../foo';
 function component () {
   var element = j('<div></div>');
 
   /* lodash is required for the next line to work */
-  element.html(_.join(['Hello','webpack'], ' '))
+  element.html(_.join(['Hello','webpack'], ' '));
 
   return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(foo);
+console.log(foo());
 
